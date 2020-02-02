d434db6836d6fe2ee87aa649d0786b09f6fae7f3
Harlan T Wood <code@harlantwood.net>
2020-02-02 00:00:32 -1000

    Uncomment tests

--- test/index.js
+++ test/index.js
@@ -40,66 +40,66 @@ const conductorConfig = Config.gen(
 );
 
 
-// orchestrator.registerScenario("Scenario1: Zome is working", async (s, t) => {
-//   const { alice, bob } = await s.players(
-//     { alice: conductorConfig, bob: conductorConfig },
-//     true
-//   );
+orchestrator.registerScenario("Scenario1: Zome is working", async (s, t) => {
+  const { alice, bob } = await s.players(
+    { alice: conductorConfig, bob: conductorConfig },
+    true
+  );
 
-//   const result_hello = await alice.call("course_dna", "courses", "hi_holo", {
-//     title: "hello holochain"
-//   })
+  const result_hello = await alice.call("course_dna", "courses", "hi_holo", {
+    title: "hello holochain"
+  })
 
-//   t.equal(result_hello.Ok, "hello holochain");
+  t.equal(result_hello.Ok, "hello holochain");
 
-//   await s.consistency();
+  await s.consistency();
 
-// });
+});
 
 
-// orchestrator.registerScenario("Scenario2: Create new course", async (scenario, t) => {
-//   const { alice, bob } = await scenario.players(
-//     { alice: conductorConfig, bob: conductorConfig },
-//     true
-//   );
+orchestrator.registerScenario("Scenario2: Create new course", async (scenario, t) => {
+  const { alice, bob } = await scenario.players(
+    { alice: conductorConfig, bob: conductorConfig },
+    true
+  );
 
-//   const title = "first course for scenario 2";
+  const title = "first course for scenario 2";
 
-//   const create_course_response = await alice.call("course_dna", "courses", "create_course", {
-//     title: title,
-//     timestamp: 1
-//   })
+  const create_course_response = await alice.call("course_dna", "courses", "create_course", {
+    title: title,
+    timestamp: 1
+  })
 
-//   const new_course_addr = create_course_response.Ok;
-//   t.ok(new_course_addr);
+  const new_course_addr = create_course_response.Ok;
+  t.ok(new_course_addr);
 
-//   await scenario.consistency();
+  await scenario.consistency();
 
-//   const get_entry_response = await alice.call("course_dna", "courses", "get_entry", {
-//     address: new_course_addr
-//   })
+  const get_entry_response = await alice.call("course_dna", "courses", "get_entry", {
+    address: new_course_addr
+  })
 
-//   t.ok(get_entry_response.Ok);
-//   t.ok(get_entry_response.Ok.App);
-//   t.ok(get_entry_response.Ok.App[1]);
+  t.ok(get_entry_response.Ok);
+  t.ok(get_entry_response.Ok.App);
+  t.ok(get_entry_response.Ok.App[1]);
 
-//   retrieved_course = JSON.parse(get_entry_response.Ok.App[1]);
-//   alice_address = alice.instance("course_dna").agentAddress;
+  retrieved_course = JSON.parse(get_entry_response.Ok.App[1]);
+  alice_address = alice.instance("course_dna").agentAddress;
 
-//   t.equal(retrieved_course.title, title);
-//   t.equal(retrieved_course.timestamp, 1);
-//   t.equal(retrieved_course.teacher_address, alice_address);
+  t.equal(retrieved_course.title, title);
+  t.equal(retrieved_course.timestamp, 1);
+  t.equal(retrieved_course.teacher_address, alice_address);
 
-//   t.deepEqual(retrieved_course, {
-//     modules: [],
-//     teacher_address: alice_address,
-//     timestamp: 1,
-//     title: 'first course for scenario 2',
-//   });
+  t.deepEqual(retrieved_course, {
+    modules: [],
+    teacher_address: alice_address,
+    timestamp: 1,
+    title: 'first course for scenario 2',
+  });
 
-//   await scenario.consistency();
+  await scenario.consistency();
 
-// });
+});
 
 
 orchestrator.registerScenario("Scenario3: Delete course", async (s, t) => {
@@ -130,6 +130,7 @@ orchestrator.registerScenario("Scenario3: Delete course", async (s, t) => {
     address: new_course_addr.Ok
   })
 
+  // prove it's actually been deleted:
   t.deepEqual(get_entry_response, { Ok: null })
 
   await s.consistency();
@@ -137,52 +138,48 @@ orchestrator.registerScenario("Scenario3: Delete course", async (s, t) => {
 });
 
 
-// orchestrator.registerScenario("Scenario4: Update course title", async (s, t) => {
-//   const { alice, bob } = await s.players(
-//     { alice: conductorConfig, bob: conductorConfig },
-//     true
-//   );
-//   const course_addr = await alice.call(
-//     "course_dna",
-//     "courses",
-//     "create_course",
-//     {
-//       title: "new course test for update test"
-//       , timestamp: 123
-//     }
-//   );
+orchestrator.registerScenario("Scenario4: Update course title", async (s, t) => {
+  const { alice, bob } = await s.players(
+    { alice: conductorConfig, bob: conductorConfig },
+    true
+  );
+  const course_addr = await alice.call(
+    "course_dna",
+    "courses",
+    "create_course",
+    {
+      title: "new course test for update test"
+      , timestamp: 123
+    }
+  );
 
-//   const course_update_addrss = await alice.call(
-//     "course_dna",
-//     "courses",
-//     "update_course",
-//     {
-//       title: "course title updated",
-//       course_address: course_addr.Ok,
-//       modules_addresses: [],
-//       timestamp: 123
+  const course_update_addrss = await alice.call(
+    "course_dna",
+    "courses",
+    "update_course",
+    {
+      title: "course title updated",
+      course_address: course_addr.Ok,
+      modules_addresses: [],
+      timestamp: 123
 
-//     }
-//   );
+    }
+  );
 
-
-
-//   const courseResult = await alice.call("course_dna", "courses", "get_entry", {
-//     address: course_update_addrss.Ok
-//   });
+  const courseResult = await alice.call("course_dna", "courses", "get_entry", {
+    address: course_update_addrss.Ok
+  });
 
 
-//   const course = JSON.parse(courseResult.Ok.App[1]);
-//   console.log(course);
-//   t.deepEqual(course, {
-//     title: "course title updated",
-//     timestamp: 123,
-//     teacher_address: alice.instance("course_dna").agentAddress,
-//     modules: []
-//   });
+  const course = JSON.parse(courseResult.Ok.App[1]);
+  console.log(course);
+  t.deepEqual(course, {
+    title: "course title updated",
+    timestamp: 123,
+    teacher_address: alice.instance("course_dna").agentAddress,
+    modules: []
+  });
 
-
-
-// });
+});
 
 orchestrator.run();
