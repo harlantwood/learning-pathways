92037e9d085cd037bf09bf97aeaa730284796017
Harlan T Wood <code@harlantwood.net>
2020-02-01 23:57:39 -1000

    homework 3

--- test/index.js
+++ test/index.js
@@ -57,79 +57,84 @@ const conductorConfig = Config.gen(
 // });
 
 
-orchestrator.registerScenario("Scenario2: Create new course", async (scenario, t) => {
-  const { alice, bob } = await scenario.players(
-    { alice: conductorConfig, bob: conductorConfig },
-    true
-  );
-
-  const title = "first course for scenario 2";
-
-  const create_course_response = await alice.call("course_dna", "courses", "create_course", {
-    title: title,
-    timestamp: 1
-  })
-
-  const new_course_addr = create_course_response.Ok;
-  t.ok(new_course_addr);
-
-  await scenario.consistency();
-
-  const get_entry_response = await alice.call("course_dna", "courses", "get_entry", {
-    address: new_course_addr
-  })
-
-  t.ok(get_entry_response.Ok);
-  t.ok(get_entry_response.Ok.App);
-  t.ok(get_entry_response.Ok.App[1]);
-
-  retrieved_course = JSON.parse(get_entry_response.Ok.App[1]);
-  alice_address = alice.instance("course_dna").agentAddress;
-
-  t.equal(retrieved_course.title, title);
-  t.equal(retrieved_course.timestamp, 1);
-  t.equal(retrieved_course.teacher_address, alice_address);
-
-  t.deepEqual(retrieved_course, {
-    modules: [],
-    teacher_address: alice_address,
-    timestamp: 1,
-    title: 'first course for scenario 2',
-  });
-
-  await scenario.consistency();
-
-});
-
-
-// orchestrator.registerScenario("Scenario3: Delete course", async (s, t) => {
-//   const { alice, bob } = await s.players(
+// orchestrator.registerScenario("Scenario2: Create new course", async (scenario, t) => {
+//   const { alice, bob } = await scenario.players(
 //     { alice: conductorConfig, bob: conductorConfig },
 //     true
 //   );
 
-//   const new_course_addr = await alice.call("course_dna", "courses", "create_course", {
-//     title: "first course for scneario 3",
+//   const title = "first course for scenario 2";
+
+//   const create_course_response = await alice.call("course_dna", "courses", "create_course", {
+//     title: title,
 //     timestamp: 1
 //   })
 
-//   t.ok(new_course_addr.Ok);
-//   await s.consistency();
+//   const new_course_addr = create_course_response.Ok;
+//   t.ok(new_course_addr);
 
-//   const deleted_course_addr = await alice.call("course_dna", "courses", "delete_course", {
-//     course_address: new_course_addr.Ok
+//   await scenario.consistency();
+
+//   const get_entry_response = await alice.call("course_dna", "courses", "get_entry", {
+//     address: new_course_addr
+//   })
+
+//   t.ok(get_entry_response.Ok);
+//   t.ok(get_entry_response.Ok.App);
+//   t.ok(get_entry_response.Ok.App[1]);
+
+//   retrieved_course = JSON.parse(get_entry_response.Ok.App[1]);
+//   alice_address = alice.instance("course_dna").agentAddress;
+
+//   t.equal(retrieved_course.title, title);
+//   t.equal(retrieved_course.timestamp, 1);
+//   t.equal(retrieved_course.teacher_address, alice_address);
+
+//   t.deepEqual(retrieved_course, {
+//     modules: [],
+//     teacher_address: alice_address,
+//     timestamp: 1,
+//     title: 'first course for scenario 2',
 //   });
 
-//   console.log("_this_is_me"); // just a trick to find the log in big out of debug test screen.
-//   console.log(deleted_course_addr);
-//   t.ok(deleted_course_addr.Ok);
-
-
-//   // TODO:Homework, get_entry and Assert that your retrived object is the samme as Created Object
-
-//   await s.consistency();
+//   await scenario.consistency();
 
 // });
+
+
+orchestrator.registerScenario("Scenario3: Delete course", async (s, t) => {
+  const { alice, bob } = await s.players(
+    { alice: conductorConfig, bob: conductorConfig },
+    true
+  );
+
+  const new_course_addr = await alice.call("course_dna", "courses", "create_course", {
+    title: "first course for scenario 3",
+    timestamp: 1
+  })
+
+  t.ok(new_course_addr.Ok);
+  await s.consistency();
+
+  const deleted_course_addr = await alice.call("course_dna", "courses", "delete_course", {
+    course_address: new_course_addr.Ok
+  });
+
+  console.log("_this_is_me"); // just a trick to find the log in big out of debug test screen.
+  console.log(deleted_course_addr);
+  t.ok(deleted_course_addr.Ok);
+
+  await s.consistency();
+
+  const get_entry_response = await alice.call("course_dna", "courses", "get_entry", {
+    address: new_course_addr.Ok
+  })
+
+  t.deepEqual(get_entry_response, { Ok: null })
+
+  await s.consistency();
+
+});
 
 
 // orchestrator.registerScenario("Scenario4: Update course title", async (s, t) => {
